b9c2a0ea493a79f1759a21155e15d78547446104
Andrey Tselishchev <vorteq@gmail.com>
2020-11-30 11:50:05 +0300

    Symlink /website/out to /docs

---
+++ docs/_next/static/webpack/pages/index.b9c44a70402975090de2.hot-update.js
@@ -0,0 +1,14 @@
+webpackHotUpdate_N_E("pages/index",{
+
+/***/ "../package.json":
+/*!***********************!*\
+  !*** ../package.json ***!
+  \***********************/
+/*! exports provided: name, version, description, keywords, homepage, repository, bugs, main, types, scripts, author, license, dependencies, devDependencies, default */
+/***/ (function(module) {
+
+eval("module.exports = JSON.parse(\"{\\\"name\\\":\\\"serialazyy\\\",\\\"version\\\":\\\"3.0.0\\\",\\\"description\\\":\\\"TypeScript class serialization / data-mapping library\\\",\\\"keywords\\\":[\\\"serialize\\\",\\\"deserialize\\\",\\\"json\\\",\\\"mapping\\\",\\\"typescript\\\"],\\\"homepage\\\":\\\"https://serialazy.teqlab.net\\\",\\\"repository\\\":{\\\"type\\\":\\\"git\\\",\\\"url\\\":\\\"https://github.com/teq/serialazy.git\\\"},\\\"bugs\\\":{\\\"url\\\":\\\"https://github.com/teq/serialazy/issues\\\"},\\\"main\\\":\\\"./lib/dist/index.js\\\",\\\"types\\\":\\\"./lib/dist/index.d.ts\\\",\\\"scripts\\\":{\\\"lint\\\":\\\"tslint -p ./lib && tslint -p ./spec && tslint -p ./samples && tslint -p ./docs\\\",\\\"clean_lib\\\":\\\"rm -rf ./lib/dist\\\",\\\"clean_spec\\\":\\\"rm -rf ./spec/bin\\\",\\\"clean_samples\\\":\\\"rm -rf ./samples/bin\\\",\\\"clean_all\\\":\\\"npm run clean_lib && npm run clean_spec && npm run clean_samples\\\",\\\"build_lib\\\":\\\"cd ./lib && tsc\\\",\\\"build_spec\\\":\\\"cd ./spec && tsc\\\",\\\"build_samples\\\":\\\"cd ./samples && tsc\\\",\\\"build_all\\\":\\\"npm run build_lib && npm run build_spec && npm run build_samples\\\",\\\"test\\\":\\\"npm run build_lib && npm run build_spec && node ./spec/bin/run.js\\\",\\\"prepublishOnly\\\":\\\"npm run lint && npm run clean_all && npm run build_all && node ./spec/bin/run.js\\\"},\\\"author\\\":\\\"Andrey Tselischev <vorteq@gmail.com>\\\",\\\"license\\\":\\\"MIT\\\",\\\"dependencies\\\":{\\\"reflect-metadata\\\":\\\"^0.1.13\\\"},\\\"devDependencies\\\":{\\\"@types/chai\\\":\\\"^4.0.4\\\",\\\"@types/chai-as-promised\\\":\\\"^7.1.0\\\",\\\"@types/mocha\\\":\\\"^5.2.5\\\",\\\"@types/node\\\":\\\"^12.12.27\\\",\\\"chai\\\":\\\"^4.1.2\\\",\\\"chai-as-promised\\\":\\\"^7.1.1\\\",\\\"mocha\\\":\\\"^7.1.1\\\",\\\"tslint\\\":\\\"^6.1.0\\\",\\\"typescript\\\":\\\"^3.7.5\\\"}}\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiIuLi9wYWNrYWdlLmpzb24uanMiLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///../package.json\n");
+
+/***/ })
+
+})
