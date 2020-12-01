75cc9b15da9d1a8393172bcc7db6f760bed714bb
Andrey Tselishchev <vorteq@gmail.com>
2020-12-01 13:12:51 +0300

    Website: Fix styles for mobile phones

--- website/components/footer.tsx
+++ website/components/footer.tsx
@@ -9,10 +9,10 @@ export function Footer({ githubLink }: Props) {
   const year = new Date().getFullYear();
 
   return (
-    <footer className="flex justify-between border-t mt-12 py-8 text-sm text-gray-600">
-      <span>© 2017 - {year} Andrey Tselishchev</span>
-      <span>Site powered by <ExternalLink href="https://nextjs.org/" newTab>next.js</ExternalLink></span>
-      { githubLink && <span>
+    <footer className="flex justify-between flex-wrap border-t mt-12 py-8 text-sm text-gray-600">
+      <span className="mx-2">© 2017 - {year} Andrey Tselishchev</span>
+      <span className="mx-2">Site powered by <ExternalLink href="https://nextjs.org/" newTab>next.js</ExternalLink></span>
+      { githubLink && <span className="mx-2">
         <ExternalLink href={githubLink} newTab>
           <img className="inline w-4 h-4 mx-1" src="/assets/icons/external-link.svg" />
           Edit on GitHub
